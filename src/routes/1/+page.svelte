a15f0c37a63ea1be7f2bbd2c538181457543a755
OniOniOn- <0onionion0@gmail.com>
2023-01-25 19:01:31 +0900

    1: dynamic attributes

--- src/routes/1/+page.svelte
+++ src/routes/1/+page.svelte
@@ -1,5 +1,10 @@
 <script>
     let name = 'svelte';
+    let src = '/tutorial/image.gif';
 </script>
 
 <h1>Hello {name.toUpperCase()}!</h1>
+
+<img src={src} alt='{name} dances.'>
+<!-- shorthand attributes: name and value are the same -->
+<img {src} alt='{name} dances.'>
