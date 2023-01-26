1f69c3ded0e85e210e2a1f4a5ffc55fec8b69f2a
OniOniOn- <0onionion0@gmail.com>
2023-01-27 03:11:01 +0900

    4: else blocks

--- src/routes/4/+page.svelte
+++ src/routes/4/+page.svelte
@@ -7,13 +7,17 @@
 </script>
 
 <!-- conditionally render markup (still adds indentation level T^T) -->
+<!--
+    # block start tag
+    : block continuation tag
+    / block end tag
+-->
 {#if user.loggedIn}
     <button on:click={toggle}>
         Log out
     </button>
-{/if}
-
-{#if !user.loggedIn}
+<!-- continue with else block -->
+{:else}
     <button on:click={toggle}>
         Log in
     </button>
