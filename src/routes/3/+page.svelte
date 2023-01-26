50b785a7168470bfa0e9a8285301e21eb959ffc3
OniOniOn- <0onionion0@gmail.com>
2023-01-27 03:02:14 +0900

    3: spread props

--- src/routes/3/+page.svelte
+++ src/routes/3/+page.svelte
@@ -1,6 +1,18 @@
 <script>
     import Nested from './Nested.svelte';
+	import Info from './Info.svelte';
+
+    const pkg = {
+        name: 'svelte',
+        version: 3,
+        speed: 'blazing',
+        website: 'https://svelte.dev'
+    };
 </script>
 
 <Nested answer={42}/>
 <Nested/>
+
+<Info name={pkg.name} version={pkg.version} speed={pkg.speed} website={pkg.website}/>
+<!-- can spread object props -->
+<Info {...pkg}/>
