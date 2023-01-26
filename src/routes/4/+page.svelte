0b3978b7f733d9ee94a8f39e569f4a33e635883e
OniOniOn- <0onionion0@gmail.com>
2023-01-27 03:19:11 +0900

    4: each blocks

--- src/routes/4/+page.svelte
+++ src/routes/4/+page.svelte
@@ -6,6 +6,12 @@
     }
 
     let x = 7;
+
+    let cats = [
+		{ id: 'J---aiyznGQ', name: 'Keyboard Cat' },
+		{ id: 'z_AbfPXTKms', name: 'Maru' },
+		{ id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat' }
+	];
 </script>
 
 <!-- conditionally render markup (still adds indentation level T^T) -->
@@ -32,3 +38,20 @@
 {:else}
 <p>{x} is between 5 and 10</p>
 {/if}
+
+<h1>The Famous Cats of YouTube</h1>
+
+<ul>
+    <!-- each blocks -->
+    {#each cats as cat, index}
+        <li><a target="_blank" href="https://www.youtube.com/watch?v={cat.id}" rel="noreferrer">
+            {cat.name}
+        </a></li>
+    {/each}
+    <!-- destructuring -->
+    {#each cats as {id, name}}
+        <li><a target="_blank" href="https://www.youtube.com/watch?v={id}" rel="noreferrer">
+            {name}
+        </a></li>
+    {/each}
+</ul>
