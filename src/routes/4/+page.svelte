070dbca82a83ec6074dc8313fbef37f34eea2200
OniOniOn- <0onionion0@gmail.com>
2023-01-27 03:10:42 +0900

    4: if blocks

---
+++ src/routes/4/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	let user = { loggedIn: false };
+
+    function toggle() {
+        user.loggedIn = !user.loggedIn;
+    }
+</script>
+
+<!-- conditionally render markup (still adds indentation level T^T) -->
+{#if user.loggedIn}
+    <button on:click={toggle}>
+        Log out
+    </button>
+{/if}
+
+{#if !user.loggedIn}
+    <button on:click={toggle}>
+        Log in
+    </button>
+{/if}
