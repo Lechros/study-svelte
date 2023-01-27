5706835cd5d5bd0460893cdfc24ff2bef4ee39fe
OniOniOn- <0onionion0@gmail.com>
2023-01-27 18:34:00 +0900

    2: reactivity review

--- src/routes/2/+page.svelte
+++ src/routes/2/+page.svelte
@@ -1,4 +1,7 @@
 <script>
+	import { set_custom_element_data } from "svelte/internal";
+
+
     let count = 0;
     // reactive declarations: auto updated when other variabled change
     $: doubled = count * 2;
@@ -33,6 +36,27 @@
     // numbers[numbers.length] = numbers.length + 1;
 
     $: sum = numbers.reduce((t, n) => t + n, 0);
+
+    let deep = {
+        first: {
+            second: {
+                arr: [1, 2, 3],
+                hello: 'world',
+            },
+            another: {
+                arr: [0, 9, 8]
+            }
+        }
+    };
+
+    function silentAdd() {
+        deep.first.second.arr.push(deep.first.second.arr.length);
+        deep.first.another.arr.push(10 - deep.first.another.arr.length);
+    }
+
+    function changeHello() {
+        deep.first.second.hello = deep.first.second.hello + "!";
+    }
 </script>
 
 <button on:click={incrementCount}>
@@ -44,3 +68,19 @@
 <p>{numbers.join(' + ')} = {sum}</p>
 
 <button on:click={addNumber}>Add a number</button>
+
+<!--
+    reactivity test
+
+    assigning to `deep.first.second.hello` triggers update
+    on every variable that is desendent of `deep`
+-->
+
+<p>{deep.first.second.arr}</p>
+<p>{deep.first.another.arr}</p>
+
+<p>{deep.first.second.hello}</p>
+
+<button on:click={silentAdd}>Silently add a number</button>
+
+<button on:click={changeHello}>Add ! to hello</button>
