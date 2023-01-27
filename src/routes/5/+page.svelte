fa1b669b02c2d412328e46343b3bd8247687f710
OniOniOn- <0onionion0@gmail.com>
2023-01-28 04:35:21 +0900

    5: event modifiers

--- src/routes/5/+page.svelte
+++ src/routes/5/+page.svelte
@@ -1,4 +1,7 @@
 <script>
+	import { prevent_default } from "svelte/internal";
+
+
     let m = { x: 0, y: 0 };
 
     function handleMousemove(event) {
@@ -6,12 +9,34 @@
         m.y = event.clientY;
 
     }
+
+    function handleClick() {
+        alert('no more alerts');
+    }
 </script>
 
 <!-- inline event handler: free syntax highlighting, no performance hit -->
 <div on:mousemove={e => m = { x: e.clientX, y: e.clientY }}>
     The mouse position is {m.x} x {m.y}
 </div>
+
+<!--
+    handler modifiers
+
+    preventDefault: calls `event.preventDefault()`
+    stopPropagation: calls `event.stopPropagation()`
+    passive: improve scroll performance (auto added)
+    nonpassive: opposite of passive
+    capture: run during the capture phase instead of the bubbling phase
+    once: remove handler after first run
+    self: only trigger if event.target is element itself
+    trusted: trigger only when `event.isTrusted` is true
+
+    chaining is allowed
+-->
+<button on:click|once={handleClick}>
+    Click me
+</button>
 
 <style>
     div {
