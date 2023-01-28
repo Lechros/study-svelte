9852a29b2efb38dda048db4edd961015333a4c03
OniOniOn- <0onionion0@gmail.com>
2023-01-28 22:04:40 +0900

    6: each block bindings

--- src/routes/6/+page.svelte
+++ src/routes/6/+page.svelte
@@ -38,6 +38,22 @@
 	}
 
 	let html = '<p>Write some text!</p>';
+
+	let todos = [
+		{ done: false, text: 'finish Svelte tutorial' },
+		{ done: false, text: 'build an app' },
+		{ done: false, text: 'world domination' }
+	];
+
+	function add() {
+		todos = todos.concat({ done: false, text: '' });
+	}
+
+	function clear() {
+		todos = todos.filter(t => !t.done);
+	}
+
+	$: remaining = todos.filter(t => !t.done).length;
 </script>
 
 <!--
@@ -159,6 +175,32 @@
 
 <pre>{html}</pre>
 
+<h1>Todos</h1>
+
+{#each todos as todo}
+	<div class:done={todo.done}>
+		<input
+			type=checkbox
+			bind:checked={todo.done}
+		>
+
+		<input
+			placeholder="What needs to be done?"
+			bind:value={todo.text}
+		>
+	</div>
+{/each}
+
+<p>{remaining} remaining</p>
+
+<button on:click={add}>
+	Add new
+</button>
+
+<button on:click={clear}>
+	Clear completed
+</button>
+
 <style>
 	label { display: flex }
 	input, p { margin: 6px }
@@ -172,5 +214,8 @@
 		padding: 0.5em;
 		border: 1px solid #eee;
 		border-radius: 4px;
+	}
+	.done {
+		opacity: 0.4;
 	}
 </style>
