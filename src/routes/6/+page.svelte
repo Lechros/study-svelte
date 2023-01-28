c96a5a0770b6406b92cf92b1992ae145eb038d51
OniOniOn- <0onionion0@gmail.com>
2023-01-28 20:05:10 +0900

    6: numeric inputs

--- src/routes/6/+page.svelte
+++ src/routes/6/+page.svelte
@@ -1,7 +1,33 @@
 <script>
     let name = 'world';
+
+	let a = 1;
+	let b = 2;
 </script>
 
 <input bind:value={name}>
 
 <h1>Hello {name}!</h1>
+
+<!--
+    svelte takes care of converting types
+    - input with type=number/range will set numeric value
+    - input with type=text will set string value
+    - two different input type might change variable type
+-->
+<label>
+	<input type=number bind:value={a} min=0 max=10>
+	<input type=range bind:value={a} min=0 max=10>
+</label>
+
+<label>
+	<input type=number bind:value={b} min=0 max=10>
+	<input type=range bind:value={b} min=0 max=10>
+</label>
+
+<p>{a} + {b} = {a + b}</p>
+
+<style>
+	label { display: flex }
+	input, p { margin: 6px }
+</style>
