214c2a839e8cca5ff19eb8ecb1e8dcb283978265
OniOniOn- <0onionion0@gmail.com>
2023-01-28 20:08:33 +0900

    6: checkbox inputs

--- src/routes/6/+page.svelte
+++ src/routes/6/+page.svelte
@@ -3,18 +3,21 @@
 
 	let a = 1;
 	let b = 2;
+
+	let yes = false;
 </script>
-
-<input bind:value={name}>
-
-<h1>Hello {name}!</h1>
 
 <!--
     svelte takes care of converting types
     - input with type=number/range will set numeric value
     - input with type=text will set string value
+    - input with type=checkbox will set boolean `checked` value
     - two different input type might change variable type
 -->
+
+<input bind:value={name}>
+
+<h1>Hello {name}!</h1>
 <label>
 	<input type=number bind:value={a} min=0 max=10>
 	<input type=range bind:value={a} min=0 max=10>
@@ -31,3 +34,18 @@
 	label { display: flex }
 	input, p { margin: 6px }
 </style>
+
+<label>
+	<input type=checkbox bind:checked={yes}>
+	Yes! Send me regular email spam
+</label>
+
+{#if yes}
+	<p>Thank you. We will bombard your inbox and sell your personal details.</p>
+{:else}
+	<p>You must opt-in to continue. If you're not paying, you're the product.</p>
+{/if}
+
+<button disabled={!yes}>
+	Subscribe
+</button>
