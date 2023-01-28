4c13414062fddae0f5c4946ad39199fdcec08b2c
OniOniOn- <0onionion0@gmail.com>
2023-01-28 21:37:27 +0900

    6: select bindings

--- src/routes/6/+page.svelte
+++ src/routes/6/+page.svelte
@@ -22,6 +22,20 @@
 
 	import { marked } from 'marked';
 	let value = `Some words are *italic*, some are **bold**`;
+
+	let questions = [
+		{ id: 1, text: `Where did you go to school?` },
+		{ id: 2, text: `What is your mother's name?` },
+		{ id: 3, text: `What is another personal fact that an attacker could easily find with Google?` }
+	];
+
+	let selected;
+
+	let answer = '';
+
+	function handleSubmit() {
+		alert(`answered question ${selected.id} (${selected.text}) with "${answer}"`);
+	}
 </script>
 
 <!--
@@ -106,8 +120,38 @@
 
 <textarea bind:value={value}></textarea>
 
+<h2>Insecurity questions</h2>
+
+<form on:submit|preventDefault={handleSubmit}>
+	<!--
+		select will set value to first in list
+		only after binding is initialized
+		before that value is `undefined`
+	-->
+	<select bind:value={selected} on:change="{() => answer = ''}">
+		{#each questions as question}
+			<option value={question}>
+				{question.text}
+			</option>
+		{/each}
+	</select>
+
+	<input bind:value={answer}>
+
+	<button disabled={!answer} type=submit>
+		Submit
+	</button>
+</form>
+
+<p>selected question {selected ? selected.id : '[waiting...]'}</p>
+
 <style>
 	label { display: flex }
 	input, p { margin: 6px }
 	textarea { width: 100%; height: 200px; }
+	input {
+		display: block;
+		width: 500px;
+		max-width: 100%;
+	}
 </style>
