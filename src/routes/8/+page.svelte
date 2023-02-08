e51e97692104c0f2607dc9f962948a7416110a0a
OniOniOn- <0onionion0@gmail.com>
2023-02-09 00:56:30 +0900

    8: custom stores

--- src/routes/8/+page.svelte
+++ src/routes/8/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import ReadableStores from "./readable-stores.svelte";
     import WritableStores from "./writable-stores.svelte";
-
+    import CustomStores from "./custom-stores.svelte";
 </script>
+
 <WritableStores/>
 <ReadableStores/>
+<CustomStores/>
