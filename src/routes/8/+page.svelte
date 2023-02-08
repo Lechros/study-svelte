c5560b3bb26d809dfc800bfe2a427afe6909f29f
OniOniOn- <0onionion0@gmail.com>
2023-02-09 00:59:10 +0900

    8: store bindings

--- src/routes/8/+page.svelte
+++ src/routes/8/+page.svelte
@@ -2,8 +2,10 @@
 	import ReadableStores from "./readable-stores.svelte";
     import WritableStores from "./writable-stores.svelte";
     import CustomStores from "./custom-stores.svelte";
+	import StoreBindings from "./store-bindings.svelte";
 </script>
 
 <WritableStores/>
 <ReadableStores/>
 <CustomStores/>
+<StoreBindings/>
